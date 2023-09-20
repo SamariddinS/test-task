87edaeafa09b967a849e6745d5ebe49fb16c5d96
SamariddinS <ssamar.96.96@gmail.com>
2023-09-20 17:20:05 +0500

    fix

--- backend/src/employee/entity/employee.entity.js
+++ backend/src/employee/entity/employee.entity.js
@@ -1,6 +1,6 @@
 //  ***   Employee Entity    ***
 
-import { boolean, minLength, number, object, safeParse, string } from 'valibot';
+import { boolean, minLength, number, object, safeParse, string, union } from 'valibot';
 import { employeeRepository } from '../repository/employee.repository.js';
 
 
@@ -9,38 +9,57 @@ export const employee = {};
 
 employee.create = async (data) => {
     const employeeSchema = object({
-        firstName: string([minLength(3)]),
-        sureName: string([minLength(3)]),
-        lastName: string([minLength(3)]),
-        birthday: string([minLength(3)]),
-        passportNumber: string([minLength(9)]),
-        phone: string([minLength(12)]),
-        secondPhone: string([minLength(12)]),
-        nationality: string([minLength(5)]),
-        country: string([minLength(5)]),
-        region: string([minLength(5)]),
-        district: string([minLength(5)]),
-        street: string([minLength(5)]),
-        homeNumber: string([minLength(2)]),
-        registrationRegion: string([minLength(5)]),
-        education: string(),
-        speciality: string([minLength(5)]),
-        maritalStatus: string(),
-        numberOfChild: number(),
-        motherFullname: string([minLength(9)]),
-        matherWorkPlace: string([minLength(9)]),
-        fatherFullname: string([minLength(9)]),
-        fatherWorkPlace: string([minLength(9)]),
-        sisterFullname: string([minLength(9)]),
-        brotherFullname: string([minLength(9)]),
-        tosAgreement: boolean()
+        firstName: string('Укажите Имя', [minLength(3, 'Минимальная длина - 3 символа')]),
+
+        sureName: string('Фамилия', [minLength(3, 'Минимальная длина - 3 символа')]),
+
+        lastName: string('Отчество', [minLength(3, 'Минимальная длина - 3 символа')]),
+
+        birthday: string('Дата рождения', [minLength(3, 'Минимальная длина - 3 символа')]),
+
+        passportNumber: string('Номер паспорта', [minLength(9, 'Минимальная длина - 9 символов')]),
+
+        phone: union([string('Телефон', [minLength(12, 'Минимальная длина - 12 символов')]), number()]),
+
+        nationality: string('Национальность', [minLength(5, 'Минимальная длина - 5 символов')]),
+
+        country: string('Страна', [minLength(5, 'Минимальная длина - 5 символов')]),
+
+        region: string('Регион', [minLength(5, 'Минимальная длина - 5 символов')]),
+
+        district: string('Район', [minLength(5, 'Минимальная длина - 5 символов')]),
+
+        street: string('Улица', [minLength(5, 'Минимальная длина - 5 символов')]),
+
+        homeNumber: union([string('Номер дома', [minLength(1, 'Минимальная длина - 1 символ')]), number()]),
+
+        registrationRegion: string('Регион регистрации', [minLength(5, 'Минимальная длина - 5 символов')]),
+
+        education: string('Образование'),
+
+        speciality: string('Специальность', [minLength(5, 'Минимальная длина - 5 символов')]),
+
+        maritalStatus: string('Семейное положение'),
+
+        numberOfChild: number('Количество детей'),
+
+        motherFullname: string('ФИО матери', [minLength(9, 'Минимальная длина - 9 символов')]),
+
+        matherWorkPlace: string('Место работы матери', [minLength(9, 'Минимальная длина - 9 символов')]),
+
+        fatherFullname: string('ФИО отца', [minLength(9, 'Минимальная длина - 9 символов')]),
+
+        fatherWorkPlace: string('Место работы отца', [minLength(9, 'Минимальная длина - 9 символов')]),
+
+        tosAgreement: boolean('Согласие с правилами использования (Правила пользовательского соглашения)'),
     });
 
+    console.log('Data', data);
     var isSchemaValid = safeParse(employeeSchema, data).success
 
     const educationId = await employeeRepository.getSelectableValueId('education_degrees', 'education_degree', data.education);
     const maritalStatusId = await employeeRepository.getSelectableValueId('marital_statuses', 'status_name', data.maritalStatus);
-
+    console.log(isSchemaValid);
     if (isSchemaValid) {
         // Make sure the user doesnt already exist
         const isExist = await employee.read({ 'passportNumber': data.passportNumber, })
